ffce73b8d13ca3c90b01851aa65bab13146080dd
DeBorjaFrancis18 <138091697+DeBorjaFrancis18@users.noreply.github.com>
2024-05-09 13:18:22 +0800

    Update SAPGUIExcelAuto.py

--- SAPGUIExcelAuto.py
+++ SAPGUIExcelAuto.py
@@ -2,23 +2,27 @@ import openpyxl
 from datetime import datetime, timedelta
 from pyrfc import Connection
 import pyautogui
+import pytesseract
 import time
+import pyperclip
+
 
 delay = 5
 short = 3
 
-time.sleep(delay)
-
-#click on the bar
-pyautogui.click(x=115, y=105)
-
-#t-code
-pyautogui.write('/nVA01')
-pyautogui.press('enter')
-
-time.sleep(delay)
-
 def create_sales_order():
+    
+    time.sleep(delay)
+
+    #click on the bar
+    pyautogui.click(x=115, y=105)
+
+    #t-code
+    pyautogui.write('/nVA01')
+    pyautogui.press('enter')
+
+    time.sleep(delay)
+
     pyautogui.write(header_info['OrderType']) 
     pyautogui.press('tab')
     pyautogui.write(header_info['SalesOrg']) 
@@ -57,30 +61,46 @@ def create_sales_order():
     #shipping info
     pyautogui.click(x=220, y=345)
     time.sleep(delay)
+    pyautogui.hotkey('ctrl', 'a')
     pyautogui.write(header_info['DeliveryDate'])
     time.sleep(short)
     pyautogui.press('tab')
     time.sleep(short)
-    pyautogui.write(header_info['DeliveryPlant'])  
+    pyautogui.write(str(header_info['DeliveryPlant']))  
     pyautogui.press('tab')
     pyautogui.hotkey('shift', 'tab')
     pyautogui.press('end')
     pyautogui.press('enter')
+    time.sleep(short)
     pyautogui.press('enter')
 
     time.sleep(delay)
+
+    print('PO Date: ' + header_info['PODate'])
+    print('PO Number: '+ header_info['PONumber'])
+    print('RDD: ' + header_info['DeliveryDate'])
 
     #materials info
-    pyautogui.write(item_info['Material']) 
-    pyautogui.press('tab')
-    pyautogui.write(item_info['Qty']) 
-    pyautogui.press('tab')
-    pyautogui.write(item_info['UoM']) 
-    pyautogui.press('tab')
+    
+    for item in item_info:
+        print(item['Material'], type(item['Material']))
+        pyautogui.write(str(item['Material']))
+        pyautogui.press('tab')
+        pyautogui.write(str(item['Qty']))  # Convert to string if necessary
+        pyautogui.press('tab')
+        pyautogui.write(item['UoM'])
+        pyautogui.press('tab')
+        pyautogui.press('enter')
+        time.sleep(delay)
 
-    pyautogui.press('enter')
-
+    pyautogui.hotkey('ctrl', 's')
     time.sleep(delay)
+
+    pyautogui.click(x=210, y=1010)
+    pyautogui.hotkey('ctrl', 'a')
+    pyautogui.hotkey('ctrl', 'c')
+    Status = pyperclip.paste()
+    print(Status)
 
 # Read Excel file
 wb = openpyxl.load_workbook('SAPAuto.xlsx')
@@ -98,7 +118,7 @@ header_info = {
     'DeliveryPlant': header_sheet['G2'].value,
     'PONumber': 'TA_' + datetime.now().strftime("%Y%m%d%H%M%S"),  # Generating PO Number
     'PODate': (datetime.now() - timedelta(days=1)).strftime("%d.%m.%y"),  # Generating PO Date
-    'DeliveryDate': (datetime.now() - timedelta(days=header_sheet['H2'].value)).strftime("%d.%m.%y"),
+    'DeliveryDate': (datetime.now() + timedelta(days=header_sheet['H2'].value)).strftime("%d.%m.%y"),
     'ShippingCondition': header_sheet['I2'].value,
     'Vendor': header_sheet['J2'].value,
 }
@@ -114,6 +134,3 @@ for row in item_sheet.iter_rows(min_row=2, values_only=True):
     })
 
 create_sales_order()
-print('PO Date: ' + header_info['PODate'])
-print('PO Number: '+ header_info['PONumber'])
-print('RDD: ' + header_info['DeliveryDate'])
